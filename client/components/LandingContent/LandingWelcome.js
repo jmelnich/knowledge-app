c7ce80d82ec3cf14778c4c8725134f18c1df3fad
Julia <julyettka@gmail.com>
2018-11-04 14:43:33 +0200

    slider

--- client/components/LandingContent/LandingWelcome.js
+++ client/components/LandingContent/LandingWelcome.js
@@ -3,11 +3,11 @@ import booksImg from "../../img/books.jpeg"
 import librImg from "../../img/welcome-bg.jpeg"
 
 class LandingWelcome extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            index: 0
-        };
+    constructor() {
+        super();
+        this.indx = 0;
+        this.imagesElems = document.getElementsByClassName("sliderImg");
+        console.log('in constructor', this.imagesElems);
         this.carousel = this.carousel.bind(this);
     }
     componentDidMount() {
@@ -16,27 +16,16 @@ class LandingWelcome extends Component {
 
 
     carousel() {
-        let x = document.getElementsByClassName("sliderImg");
-        //console.log(x);
-        for (let i = 0; i < x.length; i++) {
-            x[i].style.display = "none";
+        console.log('in carousel', this.imagesElems);
+        for (let i = 0; i < this.imagesElems.length; i++) {
+            this.imagesElems[i].style.display = "none";
         }
-        this.setState((prevState)=> ({
-            index: prevState.index + 1
-        }), () => {
-            if (this.state.index > x.length) {
-                    console.log('before',this.state.index);
-                this.setState({index: 0}), () => {
-                    console.log('after', this.state.index);
-                    x[this.state.index - 1].style.display = "block";
-                    setTimeout(this.carousel, 2500);
-                };
-            } else {
-                console.log('in else', this.state.index);
-                x[this.state.index - 1].style.display = "block";
-                setTimeout(this.carousel, 2500);
-            }
-        });
+        this.indx++;
+        if (this.indx > this.imagesElems.length) {
+            this.indx = 1;
+        }
+        this.imagesElems[this.indx - 1].style.display = "block";
+        setTimeout(this.carousel, 5500);
 
 
     }
@@ -57,20 +46,3 @@ class LandingWelcome extends Component {
     }
 }
 export default LandingWelcome;
-
-// LandingWelcome = () => {
-//     return (
-        {/*<div className="welcome">*/}
-            {/*<img src="../client/img/welcome-bg.jpeg"/>*/}
-            {/*<img src="../client/img/books.jpeg"/>*/}
-            {/*<div className="wrapper">*/}
-                {/*<div className="info flex-col">*/}
-                    {/*<h1>All knowledge in one place</h1>*/}
-                    {/*<p>Find, learn and keep records of all your skills </p>*/}
-                {/*</div>*/}
-            {/*</div>*/}
-        {/*</div>*/}
-//     );
-// }
-//
-// export default LandingWelcome;
